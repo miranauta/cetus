888788f3682c69e4719f0967154a07389e6fcdbd
Ramon Honorio <ramonaqh@gmail.com>
2019-01-07 20:49:20 -0200

    bin changes

--- scripts/svg2png.sh
+++ scripts/svg2png.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 find assets/images/svg/ -name '*.svg' -exec sh -c 'imageName=`basename "${0%}"`; npx svgexport "assets/images/svg/${imageName%}" assets/images/png/"${imageName%.*}".png' {} \;
 find assets/images/svg/ -name '*.svg' -exec sh -c 'imageName=`basename "${0%}"`; npx svgexport "assets/images/svg/${imageName%}" assets/images/png/"${imageName%.*}@2x".png 2x' {} \;
