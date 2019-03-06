615e80c7731c5b50c00b02b2eca8e3097c02ed30
Fiuzagr <fiuzagr@gmail.com>
2019-03-06 16:57:07 +0000

    Standardizes sizes

--- index.js
+++ index.js
@@ -1,46 +1,25 @@
-const _ = require("lodash");
-const StyleDictionary = require("style-dictionary").extend("./config.json");
+const _ = require('lodash');
+const StyleDictionary = require('style-dictionary').extend('./config.json');
 
 function isSize(prop) {
-  return prop.attributes.category === 'size' && !isTextSize(prop) && !isWeightSize(prop);
-}
-
-function isTextSize(prop) {
-  return prop.attributes.type === 'text' && prop.attributes.category === 'size';
-}
-
-function isWeightSize(prop) {
-  return prop.attributes.type === 'weight' && prop.attributes.category === 'size';
+  return prop.attributes.category === 'size';
 }
 
 StyleDictionary.registerTransform({
-  name: "name/ti/camel",
-  type: "name",
+  name: 'custom/name/ti/camel',
+  type: 'name',
   transformer: function(prop, options) {
     var path = prop.path.slice(1);
-    return _.camelCase([options.prefix].concat(path).join(" "));
+    return _.camelCase([options.prefix].concat(path).join(' '));
   }
 });
 
 StyleDictionary.registerTransform({
-  name: 'size/remToPt/literal',
+  name: 'custom/size/number',
   type: 'value',
-  matcher: function(prop) {
-    return isSize(prop);
-  },
+  matcher: isSize,
   transformer: function(prop) {
-    return parseFloat(prop.value, 10) * 16;
-  }
-});
-
-StyleDictionary.registerTransform({
-  name: 'size/custom/literal',
-  type: 'value',
-  matcher: function(prop) {
-    return isTextSize(prop) || isWeightSize(prop);
-  },
-  transformer: function(prop) {
-    return parseFloat(prop.value, 10) * 1.0;
+    return new Number(prop.value);
   }
 });
 
